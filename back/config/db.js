91451199244218c5b7b5584a5e41c13a7be99d00
ibrahima sory dialo <ibrahimasorysimplon@gmail.com>
2025-04-24 16:08:57 -0700

    Fix wrong casing for user model import

--- back/config/db.js
+++ back/config/db.js
@@ -3,7 +3,6 @@ const connectDB = async() =>{
     try {
         mongoose.set('strictQuery', false)
         await mongoose.connect(process.env.MONGO_URL)
-        console.log("MONGO_URL:", process.env.MONGO_URL);
         console.log('MongoDB connecteé')
     } catch (error) {
         console.log('Erreur de connexion à mongoDB:', error)
