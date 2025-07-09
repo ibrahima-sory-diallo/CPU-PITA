359f6ec3823a189c1783a6eadb144a2082de0381
ibrahima sory dialo <ibrahimasorysimplon@gmail.com>
2025-07-09 05:00:29 -0700

    mise a jour recente

--- client/src/components/Login/Logout.tsx
+++ client/src/components/Login/Logout.tsx
@@ -1,35 +1,19 @@
 import axios from "axios";
 import { FaSignOutAlt } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
-// @ts-ignore
-import Cookies from "js-cookie";
-// Définition de la fonction pour supprimer un cookie
-const removeCookie = (key: string) => {
-  if (typeof window !== "undefined") {
-    Cookies.remove(key); // Supprimer le cookie 'jwt'
-  }
-};
 
 const Logout = () => {
   const navigate = useNavigate();
 
-  // Fonction de déconnexion
   const logout = async () => {
     try {
-      // Faire une requête GET à l'API pour déconnecter l'utilisateur
-      await axios({
-        method: "get",
-        url: `${import.meta.env.VITE_API_URL}/api/user/logout`,
+      await axios.get(`${import.meta.env.VITE_API_URL}/api/user/logout`, {
         withCredentials: true,
       });
 
-      removeCookie("jwt"); // Supprimer le cookie 'jwt'
-
-      // Rediriger l'utilisateur vers /login puis recharger la page
-      navigate("/login");
-      setTimeout(() => {
-        window.location.reload();
-      }, 100); // Petite pause pour éviter des conflits avec la navigation
+      // Redirige vers la page de connexion
+      navigate("/Profil"); // ← Assure-toi que cette route existe bien
+      window.location.reload(); // Force le rechargement pour effacer les états
     } catch (err) {
       console.error("Erreur de déconnexion", err);
     }
